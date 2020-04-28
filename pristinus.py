a6786415de430b55ddfd486367a92ecb160d7a12
Philippe Bourcier <33067321+philippebourcier@users.noreply.github.com>
2020-04-28 02:37:27 +0000

    v0

--- pristinus.py
+++ pristinus.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 
 # LIBS
+import sys
 import RPi.GPIO as GPIO
 from time import sleep
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
-### VARIABLES
-LEDstatus=False
+### VARIABLES & PINS
+ON=True
+OFF=False
 # PINS FOR RELAY CONTROL
-Relays=[22,23,25]
+with open("/tmp/pristinus_relays.txt","r") as f: Relays=f.read().splitlines()
 # PIN FOR BIG RED BUTTON
-Emergency=29
+Emergency=17
 # PIN FOR DOOR SWITCH
-Door=31
+Door=18
 # DEFAULT SLEEP TIME
-sleep_d=30
+sleep_d=40
 # MAX SLEEP TIME
 sleep_max=300
-###
+##########################
 
 # apa102 : available / error / running
 def apa102(scene):
     req=Request("http://localhost/cmd?scene="+scene+"&status=start")
     try:
-        response=urlopen(req)
+        response=urlopen(req,None,3)
     except HTTPError as e:
         print('Error code: ', e.code)
     except URLError as e:
@@ -32,73 +34,67 @@ def apa102(scene):
     else:
         print(scene)
 
-# Control the relay that starts the UVC LED for a specific amount of time
-def relay(on):
-    global LEDstatus
+# sleep for some seconds...
+def cusleep():
     try:
-        if on:
-            GPIO.output(Relays,GPIO.HIGH)
-            LEDstatus=True
-            try:
-                with open("/tmp/pristinus_sleep.txt","r") as f:
-                    sleep_t=int(f.read().strip())
-                    if sleep_t>0 and sleep_t<sleep_max: sleep(sleep_t)
-                    else: sleep(sleep_d)
-            except EnvironmentError:
-                sleep(sleep_d)
-            GPIO.output(Relays,GPIO.LOW)
-        else:
-            GPIO.output(Relays,GPIO.LOW)
-            LEDstatus=False
-    except:  
-        apa102("error")
-        print("Oops, something wrong occurred!") 
-    finally:  
-    	GPIO.cleanup()
+        with open("/tmp/pristinus_sleep.txt","r") as f:
+            sleep_t=int(f.read().strip())
+            if sleep_t>=10 and sleep_t<sleep_max: sleep(sleep_t)
+            else: sleep(sleep_d)
+    except EnvironmentError:
+        sleep(sleep_d)
 
-# Callback for the 
-def uvled(why):
-    # STATE ?
-    sw_state=False
-    if GPIO.input(why):
-        sw_state=False
-        print("Switch is open.")
-    else:
-        sw_state=True
-        print("Switch is closed.")
-    # BIG RED BUTTON
-    if why==Emergency:
-        if LEDstatus:
-            relay(False)
-            apa102("error")
-            sys.exit("Now you have to reboot the whole machine...")
-    else:
-    # DOOR OPEN/CLOSED
-        if LEDstatus:
-            relay(False)
+# Control the relay that starts the UVC LED for a specific amount of time
+def relay(state):
+    try:
+        if state:
+            apa102("running")
+            GPIO.output(Relays,GPIO.HIGH)
+            print("On allume XXs les LEDs...")
+            cusleep()
+            GPIO.output(Relays,GPIO.LOW)
+            print("On éteint les LEDs")
             apa102("available")
         else:
-            relay(True)
-            apa102("running")
+            GPIO.output(Relays,GPIO.LOW)
+            print("On éteint les LEDs")
+    except:
+        apa102("error")
+        print("Oops, something wrong occurred!")
+        sys.exit("Now you have to reboot the whole machine...")
 
+def emerg_sw(who):
+    if not GPIO.input(who):
+        relay(OFF)
+        apa102("error")
+        sys.exit("Now you have to reboot the whole machine...")
+
+def door_sw(who):
+    if GPIO.input(who):
+        print("Door is closed => relay ON")
+        relay(ON)
+    else:
+        print("Door is opened => relay OFF")
+        relay(OFF)
+        apa102("available")
+
+# INIT
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
-# RELAIS
-GPIO.setup(Relays,GPIO.OUT,initial=GPIO.LOW)
 
 # COUPURE
 GPIO.setup(Emergency,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(Door,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(Emergency,GPIO.RISING,callback=uvled,bouncetime=500)
-GPIO.add_event_detect(Door,GPIO.BOTH,callback=uvled,bouncetime=500)
-
-# GREEN
+emerg_sw(Emergency)
 apa102("available")
+GPIO.add_event_detect(Emergency,GPIO.RISING,callback=emerg_sw,bouncetime=500)
+GPIO.add_event_detect(Door,GPIO.BOTH,callback=door_sw,bouncetime=500)
 
 try:
     while True:
         pass
 except KeyboardInterrupt:
     print("\nAborted by user")
+    relay(OFF)
+    GPIO.cleanup()
 
